463af8a4b00a8b717e52435c05dfffcc4d3c9c28
sangita7536 <mirashisangita712@gmail.com>
2025-08-27 20:57:49 +0530

    Add Overriding.java source file

--- OOP/src/OOP/Overriding.java
+++ OOP/src/OOP/Overriding.java
@@ -1,4 +1,38 @@
 package OOP;
+class Shape
+{
+    void show() {
+        System.out.println("Drawing shape");
+    }
+}
+
+class Circle extends Shape{
+    void show()
+    {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape{
+    void show()
+    {
+        System.out.println("Drawing Rectangle");
+    }
+}
 
 public class Overriding {
+    public static void main(String args[])
+    {
+        Shape shape = new Shape();
+        shape.show();
+
+        Circle circle = new Circle();
+        circle.show();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.show();
+
+        Shape shape1=new Circle();
+        shape1.show();
+    }
 }
