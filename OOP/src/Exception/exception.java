bc5f95a84b512d4057fb7842d6766008e2794ed8
sangita7536 <mirashisangita712@gmail.com>
2025-09-21 19:01:55 +0530

    Added exception.java

--- OOP/src/Exception/exception.java
+++ OOP/src/Exception/exception.java
@@ -1,4 +1,25 @@
 package Exception;
 
 public class exception {
+    public static void main(String args[]) throws Exception
+    {
+
+        int arr[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i]=i;
+        }
+        try
+        {
+           int res = arr[9];
+           throw new ArithmeticException("Technical error");
+        }
+        catch (ArithmeticException | ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+        }
+    }
 }
